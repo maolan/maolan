4ea957e55363bb072446e76606a13ca6828eddf8
Goran Mekić <meka@tilda.center>
2021-02-17 21:25:21 +0100

    Make the engine play/stop

--- src/desktop.cpp
+++ src/desktop.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <maolan/engine.hpp>
 #include <maolan/audio/track.hpp>
+#include <maolan/audio/clip.hpp>
 #include <maolan/audio/oss/out.hpp>
 
 #include "maolan/ui/app.hpp"
@@ -10,12 +12,14 @@ int main()
 {
   maolan::audio::OSSOut out("/dev/dsp", 2);
   maolan::audio::Track one("one", 2);
-  one.mute(true);
-  maolan::audio::Track two("two", 2);
+  maolan::audio::Clip clip("../../libmaolan/data/stereo.wav", 0, 10000000, 0, &one);
+  out.connect(&one);
 
+  maolan::Engine::init();
   maolan::UI *display = new maolan::GLFW("maolan");
   auto app = new maolan::App();
   display->run(app);
+  maolan::Engine::quit();
   delete display;
   return 0;
 }
