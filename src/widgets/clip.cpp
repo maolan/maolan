6a6d79884c87ac545c03c643c77d863733b68b97
Goran Mekić <meka@tilda.center>
2021-02-24 20:12:37 +0100

    Set clip end

--- src/widgets/clip.cpp
+++ src/widgets/clip.cpp
@@ -29,34 +29,45 @@ void Clip::draw(const ImVec2 &position, const float &h)
   const float &minHeight = state->trackMinHeight;
   const float &height = h < minHeight ? minHeight : h;
   ImDrawList *draw_list = ImGui::GetWindowDrawList();
-  ImVec4 color = { 0, 0.8, 0.8, 0.2 };
+  const ImVec4 color = { 0, 0.8, 0.8, 0.2 };
   const float start = (float)_clip->start() / (float)state->zoom;
   const float end = (float)_clip->end() / (float)state->zoom;
   const ImVec2 minimum = {position.x + start, position.y};
   const ImVec2 maximum = {position.x + end, position.y + height};
   ImVec2 size = {end - start, height};
-  const ImVec2 inner = {size.x - 6, size.y};
 
   ImGui::PushClipRect(minimum, maximum, true);
-  ImGui::SetCursorScreenPos({minimum.x + 1, minimum.y + 1});
-  ImGui::InvisibleButton(_clip->name().data(), inner);
+  ImGui::SetCursorScreenPos({minimum.x + 3, minimum.y});
+  ImGui::InvisibleButton(_clip->name().data(), {size.x - 6, size.y});
   draw_list->AddRectFilled(minimum, maximum, ImGui::ColorConvertFloat4ToU32(color), 3);
   draw_list->AddRect(minimum, maximum, ImGui::ColorConvertFloat4ToU32(ImVec4(1, 1, 1, 0.3)), 3);
   draw_list->AddText(minimum, ImGui::GetColorU32(ImGuiCol_Text), _clip->name().data());
   ImGui::PopClipRect();
 
   size.x = 3;
+  ImGui::SameLine();
+  ImGui::InvisibleButton(labels.end.data(), size);
+  auto active = ImGui::IsItemActive();
+  auto hovered = ImGui::IsItemHovered();
+  const ImGuiIO &io = ImGui::GetIO();
+  if (hovered) { ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW); }
+  if (active && io.MouseDelta.x != 0)
+  {
+    auto newEnd = _clip->end();
+    newEnd += io.MouseDelta.x * state->zoom;
+    if (newEnd <= _clip->start()) { newEnd = _clip->start() + 1; }
+    _clip->end(newEnd);
+  }
+
   ImGui::SetCursorScreenPos(minimum);
   ImGui::InvisibleButton(labels.start.data(), size);
-  const ImGuiIO &io = ImGui::GetIO();
-  bool active = ImGui::IsItemActive();
-  bool hovered = ImGui::IsItemHovered();
-  auto &delta = io.MouseDelta.x;
+  active = ImGui::IsItemActive();
+  hovered = ImGui::IsItemHovered();
   if (hovered) { ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW); }
-  if (active && delta != 0)
+  if (active && io.MouseDelta.x != 0)
   {
     auto newStart = _clip->start();
-    newStart += delta * state->zoom;
+    newStart += io.MouseDelta.x * state->zoom;
     if (newStart <= 0) { newStart = 1; }
     _clip->start(newStart);
   }
