56fa86c2609e5edafd9f26b75be7ab9daab4f0c2
Goran Mekić <meka@tilda.center>
2026-03-12 02:16:45 +0100

    Update dependencies

--- src/consts.rs
+++ src/consts.rs
@@ -61,18 +61,12 @@ pub mod ui_timing {
 }
 
 pub mod platform_caps {
-    pub const SUPPORTS_METER_POLL: bool = cfg!(any(
-        target_os = "freebsd",
-        target_os = "linux"
-    ));
-    pub const HAS_SEPARATE_AUDIO_INPUT_DEVICE: bool = cfg!(any(
-        target_os = "linux",
-        target_os = "freebsd"
-    ));
+    pub const SUPPORTS_METER_POLL: bool = cfg!(any(target_os = "freebsd", target_os = "linux"));
+    pub const HAS_SEPARATE_AUDIO_INPUT_DEVICE: bool =
+        cfg!(any(target_os = "linux", target_os = "freebsd"));
     pub const REQUIRE_SAMPLE_RATES_FOR_HW_READY: bool =
         cfg!(any(target_os = "linux", target_os = "freebsd"));
-    pub const REQUIRE_VST3_STATE_FOR_SAVE: bool =
-        cfg!(target_os = "macos");
+    pub const REQUIRE_VST3_STATE_FOR_SAVE: bool = cfg!(target_os = "macos");
     pub const SUPPORTS_LV2: bool = cfg!(all(unix, not(target_os = "macos")));
     pub const SUPPORTS_PLUGIN_GRAPH: bool = cfg!(all(unix, not(target_os = "macos")));
 }
